fd1a273885094865ecb789ff2ad184d4b988d3da
mark9134 <mark9134@gmail.com>
2019-04-08 20:51:25 -0400

    add description to HTML, UICtrlr and Prop Object

--- rental_eval_tools.js
+++ rental_eval_tools.js
@@ -1,9 +1,10 @@
 /**
  * Set of functions to calculate property evaluations
  */
-var UIController = (function() {
 
+var UIController = (function() {
     var DOMstrings = {
+        description:    'description',
         purchasePrice : 'purchase_price',
         monthlyRent : 'monthly_rent',
         annualIncome : 'annual_income',
@@ -16,6 +17,7 @@ var UIController = (function() {
     return {
         getInput : function() {
             return {
+                    description: document.getElementById(DOMstrings.description).value,
                     purchasePrice: document.getElementById(DOMstrings.purchasePrice).value,
                     monthlyRent: document.getElementById(DOMstrings.monthlyRent).value,
                     annualIncome: document.getElementById(DOMstrings.annualIncome).value,
@@ -47,7 +49,37 @@ var UIController = (function() {
 // create data model controller
 var dataController = (function() {
    
+    var Property = function (id, description, price, rent, fees) {
+        this.id = id;
+        this.description = description;
+        this.price = price;
+        this.rent = rent;
+        this.fees = fees;
+    }
+
+    propertyDB = {
+        propertyArray: []
+    }
+
+    function create_UUID(){
+        var dt = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (dt + Math.random()*16)%16 | 0;
+            dt = Math.floor(dt/16);
+            return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+        });
+        return uuid;
+    }
+
     return {
+        addNewProperty: function(desc, price, rent, fees) {
+            var id = create_UUID();
+            var newProp;
+            newProp = new Property(id, desc, price, rent, fees);
+            propertyDB.propertyArray.push(newProp);
+            console.log(propertyDB)
+            return(newProp);
+        },
         calcRentPriceRatio: function(monthlyRent, purchasePrice) {
             return (monthlyRent / purchasePrice) * 100;
             
@@ -65,23 +97,43 @@ var dataController = (function() {
 
 // create global app controller
 var controller  = (function(UICtrl, DATACtrl){
-    var dom = UICtrl.getDOMstrings;
+    
+    var setupEventListeners = function () {
+        var dom = UICtrl.getDOMstrings;
+        
+        //add event listener to button
+        document.querySelector('.calc_button').addEventListener('click',calcOnClick);
+        //add event listener for keyboard (enter key code =13)
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                calcOnClick();
+            }
+        });
+    
+    }
 
     var calcOnClick = function() {
         var properties = UICtrl.getInput();
+        var newProperty;
+
         console.log(properties);
+        newProperty = dataController.addNewProperty(properties.description,
+            properties.purchasePrice, properties.annualIncome, properties.annualExpenses);
+        
         UICtrl.displayRentRation(
             DATACtrl.calcRentPriceRatio(properties.monthlyRent, properties.purchasePrice));
         
         UICtrl.displayCapRate(DATACtrl.calCapRate(properties));
     }
+   
 
-    document.querySelector('.calc_button').addEventListener('click',calcOnClick);
-    document.addEventListener('keypress', function(event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            calcOnClick();
+    return {
+        init : function () {
+            setupEventListeners();
+
         }
-    });
-    
+    };
 
-})(UIController, dataController);
+    })(UIController, dataController);
+
+    controller.init();
